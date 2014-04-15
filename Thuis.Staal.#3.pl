aeea289951876fe2b4c8100c5a63696ea8186e65
Casper Thuis <casper.thuis@gmail.com>
2014-04-15 21:53:15 +0200

    Lekker bezig

--- Thuis.Staal.#3.pl
+++ Thuis.Staal.#3.pl
@@ -25,7 +25,6 @@ is_true( P1 or P2 ):-
     is_true( P2 ).
 
 
- hallo daniel
 /* --- A simple forward chaining rule interpreter --- */
 
 forward:-
@@ -53,3 +52,47 @@ composed_fact( Condition1 or Condition2 ):-
     composed_fact( Condition1 )
     ;
     composed_fact( Condition2 ).
+
+
+go:-
+    write( 'Geef 1 van de syntomen die hier beneden staan. Scheid de syntomen met een spatie en eidnig met een punt.' ),
+    nl,
+    write( ' koorts, hoofdpijn, diarree, bloed-ontlasting, cysten-ontlasting, jeuk,  rode-vlekken  : ' ),  
+    getsentence(Input),
+    write(Input),
+    addFacts(Input).
+    
+getsentence(Input) :- 
+    get0(Char), 
+    getrest(Char,Input). 
+
+getrest(46,[]) :-!. 
+
+getrest(32,Input) :-!, 
+    getsentence(Input). 
+
+
+getrest(Letter,[Word|Input]) :- 
+    getletters(Letter,Letters,Nextchar), 
+    name(Word,Letters), 
+
+getrest(Nextchar,Input). 
+
+getletters(46,[],46):-!. 
+getletters(32,[],32):-!. 
+
+getletters(Let,[Let|Letters],Nextchar) :- 
+    get0(Char), 
+    getletters(Char,Letters,Nextchar).
+
+
+if koorts then malaria. 
+if hogekoorts then malariatertiana.    
+    
+ 
+
+addFacts([]):- forward. 
+
+addFacts([H|List]):- 
+    assert(fact(H)), 
+    addFacts(List).
