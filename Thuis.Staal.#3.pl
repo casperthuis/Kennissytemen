60d1fcdab13b0cffd7f78fabe3ec06d3d8a9c503
danielstaal <daniel.staal@hotmail.com>
2014-04-17 12:33:48 +0200

    go functie

--- Thuis.Staal.#3.pl
+++ Thuis.Staal.#3.pl
@@ -1,3 +1,4 @@
+
 /* --- Defining operators --- */
 
 :- op(800, fx, if).
@@ -52,4 +53,62 @@ composed_fact( Condition1 or Condition2 ):-
     composed_fact( Condition1 )
     ;
     composed_fact( Condition2 ).
-    
+
+
+go:-
+    write( 'Geef 1 van de syntomen die hier beneden staan. Scheid de syntomen met een spatie en eidnig met een punt.' ),
+    nl,
+    symptomsSuperclasses(List),
+    write(List),
+    getsentence(Input),
+    write(Input),
+    addFacts(Input).
+
+symptomsSuperclasses([X,Y,Z]):-
+    if X then malaria,
+    if Y then darminfectie,
+    if Z then huidziekte.
+
+getsentence(Input) :- 
+    get0(Char), 
+    getrest(Char,Input). 
+
+getrest(46,[]) :-!. 
+
+getrest(32,Input) :-!, 
+    getsentence(Input). 
+
+
+getrest(Letter,[Word|Input]) :- 
+    getletters(Letter,Letters,Nextchar), 
+    name(Word,Letters), 
+
+getrest(Nextchar,Input). 
+
+getletters(46,[],46):-!. 
+getletters(32,[],32):-!. 
+
+getletters(Let,[Let|Letters],Nextchar) :- 
+    get0(Char), 
+    getletters(Char,Letters,Nextchar).
+
+if koorts then malaria.
+if diarree then darminfectie.
+if jeuk then huidziekte.
+
+if malaria then malaria-tertiana or malaria-tropica.
+
+if hoge-koorts and hoge-pieken then malaria-tertiana.
+if hoge-koorts and 3-dagen-koorts then malaria-tropica.
+if hoge-koorts and diarree-perdag and hevige-krampen then bacillaire-dysenterie.
+if bloedslijm and diarree and cysten then amoeben-dysenterie. %vraag over diarree 
+if hoge-koorts and diarree then tyfus.
+if rode-jeukende-plekken and licht-schilferende-huid and jeuk then schimmels.
+if jeukende-huiduitslag and jeuk then mijten.
+if jeukende-rode-pukkels and jeuk then prickly-heat.    
+
+addFacts([]):- forward. 
+
+addFacts([H|List]):- 
+    assert(fact(H)), 
+    addFacts(List).
