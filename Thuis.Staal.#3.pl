0269f97ede656de07ddf665f2c85ab4df9c184f7
Casper Thuis <casper.thuis@gmail.com>
2014-04-18 18:08:24 +0200

    comments erbij

--- Thuis.Staal.#3.pl
+++ Thuis.Staal.#3.pl
@@ -1,6 +1,25 @@
 :- dynamic fact/1.
 
-fact(a).
+% Superclass stands in front, if we need all the properties from a refined
+% disease you remove the first propertie and we are left whit all the
+% properties of a disease  
+if koorts or gebeten then malaria.
+if diarree or koorts then darminfectie.
+if jeuk then huidziekte.
+if malaria and hogekoorts and hogepieken then malariatertiana.
+if malaria and hogekoorts and dagenkoorts then malariatropica.
+if darminfectie and hogekoorts and diarreeperdag and hevigekrampen then bacillairedysenterie.
+if darminfectie and bloedslijm and diarree and cysten then amoebendysenterie. %vraag over diarree 
+if darminfectie and hogekoorts and diarree then tyfus.
+if huidziekte and rodejeukendeplekken and lichtschilferendehuid and jeuk then schimmels.
+if huidziekte and jeukendehuiduitslag and jeuk then mijten.
+if huidziekte and jeukenderodepukkels and jeuk then pricklyheat.    
+
+%List all superdiseases to seprate it whit refined disease.
+sd(malaria).
+sd(darminfectie).
+sd(huidziekte).
+
 
 /* --- Defining operators --- */
 
@@ -57,6 +76,14 @@ composed_fact( Condition1 or Condition2 ):-
     ;
     composed_fact( Condition2 ).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% The main function it doesn't do the data refinement   %
+% since we let the user input the symptoms that are     %
+% are needed to match it whit a superclass in the       %
+% disease tree.'                                        %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 go:-
     write( 'Geef een aantal van de symptomen die hier beneden staan. Scheid de symptomen met een spatie en eindig met een punt.' ),
@@ -68,17 +95,12 @@ go:-
     addFacts(Input),
     questions.
 
-writeSymptoms([]).
-
-writeSymptoms([FirstSymptom|List]):-
-    write('Heb je dit symptoom : '),
-    write(FirstSymptom),
-    nl,
-    writeSymptoms(List).
-
-sd(malaria).
-sd(darminfectie).
-sd(huidziekte).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% find all the properties of the superclasses and returns%
+% a list of them.                                        %                                                       
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 symptomsSuperclasses(List):-
     findall(Superclasses, sd(Superclasses), ListSup),
@@ -92,6 +114,28 @@ findPropSuperclasses([H|Rest], [X|List]):-
     if X then H,
     findPropSuperclasses(Rest, List).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% writes down symptoms to interface.                    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+writeSymptoms([]).
+
+writeSymptoms([FirstSymptom|List]):-
+    write('Heb je dit symptoom : '),
+    write(FirstSymptom),
+    nl,
+    writeSymptoms(List).
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% toAtoms removes the list of all the and and or operaters%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 toAtoms([], []).
 
 toAtoms([H|Rest], [H|List]):-
@@ -103,26 +147,28 @@ toAtoms([H|Rest], List):-
     H = X or Y), 
     toAtoms([X,Y|Rest], List).
 
-
-%Superclass staat vooraan
-if koorts or gebeten then malaria.
-if diarree or koorts then darminfectie.
-if jeuk then huidziekte.
-if malaria and hogekoorts and hogepieken then malariatertiana.
-if malaria and hogekoorts and dagenkoorts then malariatropica.
-if darminfectie and hogekoorts and diarreeperdag and hevigekrampen then bacillairedysenterie.
-if darminfectie and bloedslijm and diarree and cysten then amoebendysenterie. %vraag over diarree 
-if darminfectie and hogekoorts and diarree then tyfus.
-if huidziekte and rodejeukendeplekken and lichtschilferendehuid and jeuk then schimmels.
-if huidziekte and jeukendehuiduitslag and jeuk then mijten.
-if huidziekte and jeukenderodepukkels and jeuk then pricklyheat.    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Questions predicate returns all the questions that    %
+% are relavant given the active superclasses. The       %
+% superclasses  are active when a user gives the        %
+% symptoms that relate whit that superclass. After that %
+% the questions are send to askQuestions(to transfer)   %
+% the properties of a disease into a question until     % 
+% either a disease is found or the questions run out    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
    
-% Search all facts and 
+ 
 questions:-
     findall(Superclasses, fact(Superclasses), ListFacts),
     checkSuperclasses(ListFacts, ListSups),
     checkSups(ListSups).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Checks whether the fact is a superclass if it is it add%
+% the Fact                                              % 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 checkSuperclasses([],[]).
 
 checkSuperclasses([H|Rest], [H|List]):-
@@ -132,6 +178,12 @@ checkSuperclasses([H|Rest], [H|List]):-
 checkSuperclasses([_|Rest], List):-
     checkSuperclasses(Rest, List).
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Checks whether the fact is a superclass if it is it add%
+% the Fact                                              % 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 checkSups([]):- fail.
 
 checkSups([H|_]):-
@@ -139,20 +191,22 @@ checkSups([H|_]):-
 
 checkSups([_|Rest]):-
     checkSups(Rest).
-
+ 
 askQuestions(Superclass):-
     getDecomposedLists(Superclass, List),
     findDisease(List).
 
-%Vindt een ziekte als het een fact is en deze fact niet als premisse 
-%bekend is maar wel als een goal.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% This predicate is the refinement process, it       %
+%% continues till either the list is empty or the     %
+%% disease is found                                   % 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 findDisease([]):- fail.
 
 findDisease([H|_]):-
-
-    %Slimme vragen stellen.
-
     writeSymptoms(H),    
     getsentence(Input),
     write(Input),
@@ -161,6 +215,17 @@ findDisease([H|_]):-
 
 findDisease([_|List]):-
     findDisease(List).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% checkForDisease together whit searchDisease checks   %
+%% whit the whether a newly added fact is disease. It   %
+%% does this by check whether the fact is End Conclusion%
+%% and it check is it not in the premises. It also      %
+%% if its not a superclass. If all checks out it writes % 
+%% the disease                                          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 checkForDisease:-
     findall(Facts, fact(Facts), ListOfFacts),
@@ -184,9 +249,26 @@ searchDisease([H|_]):-
 searchDisease([_|Rest]):-
     searchDisease(Rest).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+%% getDecomposedLists you give a Superclass and         %%
+%%returns a List whit all the premises where superclass %%
+%% is is part of the premise, in other words it gives all%
+%% the properties of all the diseases that fall under   %%
+%% the superclass                                       %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 getDecomposedLists(Superclass, DecList):-
     findall(Premises, if Premises then _, List),
     decomposeList(Superclass, List, DecList).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% decomposeList gets a list a decomopeses all the      %
+%% and , or operators out of it and returns a list again%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 decomposeList(_, [], []).
 
@@ -198,13 +280,25 @@ decomposeList(Superclass, [H|List], [Rest|DecList]):-
 decomposeList(Superclass, [_|List], DecList):-
     decomposeList(Superclass, List, DecList).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% addFacts get a list of symptoms and add them as      %%
+%% as fact when the list is empty                       %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 addFacts([]):- forward.
 
 addFacts([H|List]):- 
     assert(fact(H)), 
     addFacts(List).
 
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% getsentence is a predicate from bratko we use to  %%
+%% get the user input from and return it into a list %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 getsentence(Input) :- 
     get0(Char), 
     getrest(Char,Input). 
